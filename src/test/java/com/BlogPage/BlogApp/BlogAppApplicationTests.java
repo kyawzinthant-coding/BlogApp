0c775178485a89306f029b2af8af31f73732fcd0
Kyaw Zin Thant <kyawthantver2@gmail.com>
2024-05-09 01:52:21 +0630

    project init

---
+++ src/test/java/com/BlogPage/BlogApp/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogPage.BlogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
